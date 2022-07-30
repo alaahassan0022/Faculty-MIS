7622864ded10917d5449ac6b420bd1c9edce2a60
alaahassan0022 <110051703+alaahassan0022@users.noreply.github.com>
2022-07-30 13:31:54 +0200

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SWE_Project
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void userToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void yourDepartmentCoursesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            show_available_courses show_Available_Courses = new show_available_courses();
+            show_Available_Courses.Show();
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+            if (!login.staff)
+            {
+                reportsToolStripMenuItem.Visible = false;
+                manageInfoToolStripMenuItem.Visible = false;
+            }
+            else
+            {
+                staffToolStripMenuItem.Visible = false;
+                yourDepartmentCoursesToolStripMenuItem.Visible = false;
+            }
+        }
+
+        private void staffToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reportsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void manageInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void displayInformationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            user_information ui = new user_information();
+            ui.Show();
+
+        }
+
+        private void searchStaffToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            search_staff s = new search_staff();
+            s.Show();
+        }
+
+        private void studentDeptToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            stud_dept s = new stud_dept();
+            s.Show();
+        }
+
+        private void coursesDeptToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            course_dept cd = new course_dept();
+            cd.Show();
+        }
+
+        private void manageStudentAndStaffInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            view_info___discon v = new view_info___discon();
+            v.Show();
+        }
+
+        private void manageStudentsInDepartmentsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            display_studs_in_dept___discon dsd = new display_studs_in_dept___discon();
+            dsd.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ register.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+namespace SWE_Project
+{
+    public partial class register : Form
+    {
+        int dno;
+        public static int x = 0;
+
+        string data = "Data source = orcl; User Id = scott; Password=tiger";
+        OracleConnection conn;
+        public register()
+        {
+            InitializeComponent();
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            conn = new OracleConnection(data);
+            conn.Open();
+            label7.Hide();
+            textBox7.Hide();
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select deptname from cdept";
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+                   comboBox1.Items.Add(dr[0].ToString());
+            dr.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OracleCommand c = new OracleCommand();
+            c.Connection = conn;
+            x = 0;
+            if (!checkBox1.Checked) //stud
+            {
+                x = 1;
+            
+                c.CommandText = "insert into student values (:fisrtname , :lastname ,:password , :email , :id , :dept )";
+                
+                c.Parameters.Add("firstname", textBox1.Text);
+                c.Parameters.Add("lastname", textBox2.Text);
+                c.Parameters.Add("password", textBox3.Text);
+                c.Parameters.Add("email", textBox4.Text);
+
+                c.Parameters.Add("id", int.Parse(textBox5.Text));
+                c.Parameters.Add("dept", dno);
+
+                // id is unique
+                // dept must be found 
+            }
+
+            else
+            {
+                c.CommandText = "insert into staff values (:fisrtname , :lastname ,:password , :email , :id, :rank)";
+                c.Parameters.Add("firstname", textBox1.Text);
+                c.Parameters.Add("lastname", textBox2.Text);
+                c.Parameters.Add("password", textBox3.Text);
+                c.Parameters.Add("email", textBox4.Text);
+                c.Parameters.Add("id", int.Parse(textBox5.Text));
+                c.Parameters.Add("rank", textBox7.Text);
+            }
+
+
+
+
+
+            c.CommandType = CommandType.Text;
+            int r = c.ExecuteNonQuery();
+            if (r != -1)
+            {
+                MessageBox.Show("User Registered Successfully");
+                this.Hide();
+            }
+        }
+
+        private void Form4_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            conn.Dispose();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                label7.Show();
+                textBox7.Show();
+                label6.Hide();
+                comboBox1.Hide();
+            }
+            else {
+                label7.Hide();
+                textBox7.Hide();
+                label6.Show();
+                comboBox1.Show();
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select deptnum from cdept where deptname =:d";
+            cmd.Parameters.Add("d", comboBox1.SelectedItem.ToString());
+            OracleDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+                dno = int.Parse(dr[0].ToString());
+            dr.Close();
+        }
+    }
+}
